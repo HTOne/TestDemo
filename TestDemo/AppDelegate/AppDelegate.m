7b28016a651a131589b99c6f7e24c363c4bdf7c7
万海堂 <wanhaitang@sinter.cn>
2017-11-27 20:26:36 +0800

    Ceshi1

--- TestDemo/AppDelegate/AppDelegate.m
+++ TestDemo/AppDelegate/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     NSLog(@"ceshi");
+    NSLog(@"test1");
     //初始化window
     [self initWindow];
     
