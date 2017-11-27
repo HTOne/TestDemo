3d25a2d21f37cac671a32ffba308cab386c9266f
万海堂 <wanhaitang@sinter.cn>
2017-11-27 20:28:04 +0800

    master

--- TestDemo/AppDelegate/AppDelegate.m
+++ TestDemo/AppDelegate/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     NSLog(@"ceshi");
+    NSLog(@"master");
     //初始化window
     [self initWindow];
     
